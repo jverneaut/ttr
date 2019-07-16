c3ec1b8cbbb3589526786e17298e5d019b5c0b82
jverneaut <jverneaut@gmail.com>
2019-07-16 12:36:57 +0200

    Add dollar

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo "Copying files to droplet..."
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@SERVER_URL:/var/www
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@$SERVER_URL:/var/www
 
 echo "Copying files to droplet: OK"
 
 echo "Restarting server..."
 
-ssh root@SERVER_URL "pm2 kill; cd /var/www/ttr; pm2 start index.js"
+ssh root@$SERVER_URL "pm2 kill; cd /var/www/ttr; pm2 start index.js"
 
 echo "Restarting server: OK"
 
