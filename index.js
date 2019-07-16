c3af3d00e68a45d8ce12fc6ae5b6e4900328af9f
jverneaut <jverneaut@gmail.com>
2019-07-16 12:00:01 +0200

    Make changes

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/api', (req, res) => {
+  res.json({
+    posts: [
+      { titre: 'Post 1' },
+      { titre: 'Post 2' },
+      { titre: 'Post 3' },
+    ]
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
