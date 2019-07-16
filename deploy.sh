127312646b164b26c8bbbc329499e49e9765a4c9
jverneaut <jverneaut@gmail.com>
2019-07-16 12:40:38 +0200

    Revert dollar

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo "Copying files to droplet..."
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@$SERVER_URL:/var/www
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@207.154.227.133:/var/www
 
 echo "Copying files to droplet: OK"
 
 echo "Restarting server..."
 
-ssh root@$SERVER_URL "pm2 kill; cd /var/www/ttr; pm2 start index.js"
+ssh root@207.154.227.133 "pm2 kill; cd /var/www/ttr; pm2 start index.js"
 
 echo "Restarting server: OK"
 
